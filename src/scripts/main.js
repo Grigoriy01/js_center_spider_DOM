164277357b7236334fd4903de3045c76a5063c12
Grigoriy <m9258923028@gmail.com>
2026-03-25 18:52:34 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const weallSize = 400;
+
+spider.style.top = `${weallSize / 2 - 50 / 2}px `;
+spider.style.left = `${weallSize / 2 - 50 / 2}px `;
